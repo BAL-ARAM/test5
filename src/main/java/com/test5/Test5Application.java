2f944e2807e8e9c08224f623b8670d6b99d747fc
BAL-ARAM <riderbalaram923@gmail.com>
2025-03-15 17:04:35 +0530

    first commit

---
+++ src/main/java/com/test5/Test5Application.java
@@ -0,0 +1,15 @@
+package com.test5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test5Application {
+
+	public static void main(String[] args) {
+
+		int x=10;
+			SpringApplication.run(Test5Application.class, args);
+	}
+
+}
